3d7a40836ccc5b301a151c13b5b61ccd49e9b952
arnab-dev-dotcom <a2000.chowdhury@gmail.com>
2025-04-12 17:39:50 +0530

    our js files are add

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Javascript
+Hi, Guys
 You can learn our scripting tutorial easily. Thankyou<br>
 <p><b>Author: Arnab Chowdhury</b></p>
---
+++ module/add.js
@@ -0,0 +1,13 @@
+export const add = (a, b, c) => {
+  return a + b + c;
+};
+
+// math.js
+function multiply(a, b) {
+  return a * b;
+}
+
+let name = "Arnab Chowdhury";
+
+export default { multiply, name };//here export as object
+//export keyword help to export the function
